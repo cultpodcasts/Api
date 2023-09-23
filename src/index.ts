45c9d3d095f67bf7689cc3e2bed2e46407ea6ee2
Jon Breen <jonbreen@outlook.com>
2023-09-23 11:45:22 +0100

    Removing cron job

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 export interface Env {
-		// If you set another name in wrangler.toml as the value for 'binding',
-		// replace "DB" with the variable name you defined.
 		DB: D1Database;
 }
+
+//const cache = caches.default;
+const homepageApiUrl = "https://api-cultpodcasts.azurewebsites.net/api/homepage";
+const homepageRequest = new Request(homepageApiUrl);
 
 export default {
 		async fetch(request: Request, env: Env) {
 				const { pathname } = new URL(request.url);
-				const route = "/api/episode_search";
+				const searchRoute = "/api/episode_search";
 				const corsHeaders = {
 						"Access-Control-Allow-Origin": "*",//"https://cultpodcasts.com",
 						"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
@@ -23,13 +25,13 @@ export default {
 						});
 				}
 
-				if (pathname.startsWith(route)) {
-						var searchTerms = decodeURIComponent(pathname.substring(route.length+1));
+				if (pathname.startsWith(searchRoute)) {
+						var searchTerms = decodeURIComponent(pathname.substring(searchRoute.length + 1));
 
 
 						// If you did not use `DB` as your binding name, change it here
 						const { results } = await env.DB.prepare(
-						`
+								`
 								SELECT p.Name AS Podcast,
 								       p.Publisher,
 											 e.Title,
@@ -57,12 +59,57 @@ export default {
 								});
 				}
 
+				if (pathname.startsWith("/api/homepage")) {
+						//return cacheHomepage();
+						let response = await fetch(homepageRequest, {
+								cf: {
+										cacheTtl: 600,
+										cacheEverything: true
+								}
+						});
+						response = new Response(response.body, response);
+						response.headers.set("Cache-Control", "max-age=600");
+						response.headers.append("Access-Control-Allow-Origin", "*");
+						response.headers.append("Access-Control-Allow-Methods", "GET,OPTIONS");
+						return response;
+				}
+
 				return new Response(
 						"Call /api/episode_search to search episode descriptions",
 						{
 								headers: {
 										...corsHeaders
 								}
-						});
+						});//check
 		},
+
+		//async scheduled(
+		//		event: ScheduledEvent,
+		//		env: Env,
+		//		ctx: EventContext<Env, any, any>
+		//) {
+		//		ctx.waitUntil(handleSchedule(event, env));
+		//},
 };
+
+//function handleSchedule(
+//		event: ScheduledEvent,
+//		env: Env
+//): Promise < Response > {
+
+//		return cacheHomepage();
+//}
+
+// async function cacheHomepage(): Promise<Response> {
+//		let response = await fetch(homepageRequest, {
+//				cf: {
+//						cacheTtl: 600,
+//						cacheEverything: true
+//				}
+//		});
+//		response = new Response(response.body, response);
+//		response.headers.set("Cache-Control", "max-age=600");
+//		response.headers.append("Access-Control-Allow-Origin", "*");
+//		response.headers.append("Access-Control-Allow-Methods", "GET,OPTIONS");
+//		return response
+//}
