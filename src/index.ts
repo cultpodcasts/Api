ccd3cbf12c43d7d8bcd901a4caf6356e1a2784d5
Jon Breen <jonbreen@outlook.com>
2024-01-05 17:29:13 +0000

    Current version

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export default {
 						"Access-Control-Allow-Origin": "*", //"https://cultpodcasts.com",
 						"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
 						"Access-Control-Max-Age": "86400",
+						"Access-Control-Allow-Headers": "content-type"
 				};
 
 				if (request.method === "OPTIONS") {
@@ -108,30 +109,80 @@ export default {
 				}
 
 				if (pathname.startsWith(azureSearchRoute)) {
-						const searchPath = pathname.substring(azureSearchRoute.length + 1);
-						const API_HOST = "https://cultpodcasts.search.windows.net/indexes/cultpodcasts-two/docs?api-version=2023-07-01-Preview&";
-						const url = `${API_HOST}${searchPath}${searchParams}`;
-						//return new Response(url);
+						if (request.method == "GET") {
+								const searchPath = pathname.substring(azureSearchRoute.length + 1);
+								const API_HOST = "https://cultpodcasts.search.windows.net/indexes/cultpodcasts-two/docs?api-version=2023-07-01-Preview&";
+								const url = `${API_HOST}${searchPath}${searchParams}`;
+								//return new Response(url);
 
-						let response = await fetch(url, {
-								cf: {
-										cacheEverything: true,
-										cacheTtl: 600
-								},
-								headers: {
-										"api-key": "TBapMt2RTuulXdyMMICzPK5Jk2HyHNUXKhWX9Sex9IAzSeBS5J1Z",
+								let response = await fetch(url, {
+										cf: {
+												cacheEverything: true,
+												cacheTtl: 600
+										},
+										headers: {
+												"api-key": "TBapMt2RTuulXdyMMICzPK5Jk2HyHNUXKhWX9Sex9IAzSeBS5J1Z",
+										}
+								});
+								let body: any = await response.json();
+								body["@odata.context"] = null;
+
+								const headers = new Headers();
+								headers.set("Cache-Control", "max-age=600");
+								headers.append("Content-Type", "application/json");
+								headers.append("Access-Control-Allow-Origin", "*");
+								headers.append("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+								headers.append("x-flow", "1");
+
+								return new Response(JSON.stringify(body), { headers })
+						} else if (request.method == "POST") {
+								console.log("In post request");
+								const API_HOST = "https://cultpodcasts.search.windows.net/indexes/cultpodcasts-two/docs/search?api-version=2023-07-01-Preview";
+								const url = `${API_HOST}`;
+								console.log("Constructucted url: " + url);
+
+								var requestBody:any =  request.body;
+								let response = await fetch(url, {
+										cf: {
+												cacheEverything: true,
+												cacheTtl: 600
+										},
+										headers: {
+												"api-key": "TBapMt2RTuulXdyMMICzPK5Jk2HyHNUXKhWX9Sex9IAzSeBS5J1Z",
+												"content-type": "application/json;charset=UTF-8",
+										},
+										body: requestBody,
+										method: "POST"
+								});
+								console.log("Executed fetch");
+								console.log("Fetch response code:" + response.status);
+
+								const headers = new Headers();
+								headers.set("Cache-Control", "max-age=600");
+								headers.append("Content-Type", "application/json");
+								headers.append("Access-Control-Allow-Origin", "*");
+								headers.append("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+								headers.append("x-flow", "2");
+								console.log("Set headers");
+
+
+								if (response.status !=200) {
+										console.log("Remote error")
+										return new Response(response.body, { headers, status: response.status })
 								}
-						});
-						let body:any = await response.json();
-						body["@odata.context"] = null;
 
-						const headers = new Headers();
-						headers.set("Cache-Control", "max-age=600");
-						headers.append("Content-Type", "application/json");
-						headers.append("Access-Control-Allow-Origin", "*");
-						headers.append("Access-Control-Allow-Methods", "GET,OPTIONS");
+								let body: any = await response.json();
+								console.log("Loaded body");
 
-						return new Response(JSON.stringify(body), {headers})
+								body["@odata.context"] = null;
+								console.log("Scrubbed odata-context");
+
+								var bodyJson = JSON.stringify(body);
+								console.log("Stringified body");
+								console.log(bodyJson);
+
+								return new Response(bodyJson, { headers })
+						}
 				}
 
 				return new Response(
