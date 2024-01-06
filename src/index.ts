6a6902c37b3b725a6a921a65ec1ccc85b7b41ede
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-01-06 14:34:27 +0000

    Feature/analytics (#3)
    * Current version
    
    * Clean up
    
    * Added search analytics
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,24 @@
 export interface Env {
-		DB: D1Database;
 		Content: R2Bucket;
+		Analytics: AnalyticsEngineDataset;
 }
 
 export default {
 		async fetch(request: Request, env: Env) {
 				const { pathname, searchParams } = new URL(request.url);
-				const searchRoute = "/api/episode_search";
 				const homeRoute = "/homepage";
-				const azureSearchRoute = "/api";
-				const sqlLimit = 20;
+				const searchRoute = "/api";
 				const corsHeaders = {
 						"Access-Control-Allow-Origin": "*", //"https://cultpodcasts.com",
 						"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
 						"Access-Control-Max-Age": "86400",
+						"Access-Control-Allow-Headers": "content-type"
 				};
 
 				if (request.method === "OPTIONS") {
-						// Handle CORS preflight requests
 						return new Response(null, {
 								headers: {
 										...corsHeaders,
-								},
-						});
-				}
-
-				if (pathname.startsWith(searchRoute)) {
-						var searchTerms = decodeURIComponent(
-								pathname.substring(searchRoute.length + 1)
-						);
-						let pageSql = "";
-						const pageParam: string = "page";
-						if (searchParams.has(pageParam)) {
-								var pageSearchComponent = searchParams.get(pageParam);
-								if (pageSearchComponent) {
-										var page: number = parseInt(pageSearchComponent);
-										pageSql = ` OFFSET ${(page - 1) * sqlLimit}`;
-								}
-						}
-
-						let orderSql = "rank";
-						const orderParam: string = "order";
-						if (searchParams.has(orderParam)) {
-								var orderSearchComponent = searchParams.get(orderParam);
-								if (orderSearchComponent) {
-										switch (orderSearchComponent) {
-												case "date":
-														orderSql = "release";
-														break;
-												case "date-desc":
-														orderSql = "release DESC";
-														break;
-												default:
-														orderSql;
-														break;
-										}
-								}
-						}
-
-						const { results } = await env.DB.prepare(
-								`
-								SELECT p.Name AS podcastName,
-								       p.Publisher AS publisher,
-											 e.Title AS episodeTitle,
-											 e.Description AS episodeDescription,
-											 e.Release AS release,
-											 time(e.Length) AS length,
-											 e.Explicit AS explicit,
-											 e.Spotify AS spotify,
-											 e.Apple AS apple,
-											 e.YouTube AS youtube
-								FROM EpisodesText t
-								JOIN Episodes e ON t.Guid = e.Guid
-								JOIN Podcasts p ON e.PodcastId = p.Id
-								WHERE EpisodesText MATCH ?
-								ORDER BY ${orderSql}
-								LIMIT ${sqlLimit}${pageSql}
-						`
-						)
-								.bind(searchTerms)
-								.all();
-						return Response.json(results, {
-								headers: {
-										...corsHeaders,
-										"Cache-Control": "max-age=600"
 								},
 						});
 				}
@@ -102,41 +37,89 @@ export default {
 						headers.append("Access-Control-Allow-Origin", "*");
 						headers.append("Access-Control-Allow-Methods", "GET,OPTIONS");
 
-						return new Response(object.body, {
-								headers
-						});
+						return new Response(object.body, { headers });
 				}
 
-				if (pathname.startsWith(azureSearchRoute)) {
-						const searchPath = pathname.substring(azureSearchRoute.length + 1);
-						const API_HOST = "https://cultpodcasts.search.windows.net/indexes/cultpodcasts-two/docs?api-version=2023-07-01-Preview&";
-						const url = `${API_HOST}${searchPath}${searchParams}`;
-						//return new Response(url);
+				if (pathname.startsWith(searchRoute)) {
+						const API_HOST = "https://cultpodcasts.search.windows.net/indexes/cultpodcasts-two/docs/search?api-version=2023-07-01-Preview";
+						const url = `${API_HOST}`;
 
-						let response = await fetch(url, {
-								cf: {
-										cacheEverything: true,
-										cacheTtl: 600
-								},
-								headers: {
-										"api-key": "TBapMt2RTuulXdyMMICzPK5Jk2HyHNUXKhWX9Sex9IAzSeBS5J1Z",
-								}
-						});
-						let body:any = await response.json();
-						body["@odata.context"] = null;
+						var dataPoint: AnalyticsEngineDataPoint | undefined;
+						return request
+								.json()
+								.then(async (data: any) => {
+										var requestBody = JSON.stringify(data);
+										if (data.search) {
+												//console.log("Search: " + data.search);
+												dataPoint = { indexes: [data.search], blobs: [data.search, "search"] };
+										} else if (data.filter) {
+												var filter: string = data.filter;
+												//console.log("Filter: " + filter);
+												if (filter.indexOf("(podcastName eq '") == 0) {
+														var query = filter.slice(17, -2);
+														//console.log("Podcast name: " + query);
+														dataPoint = { indexes: [query], blobs: [query, "podcast"] };
+												} else if (filter.indexOf("subjects/any(s: s eq '") == 0) {
+														var query = filter.slice(22, - 2);
+														//console.log("Subject: " + query);
+														dataPoint = { indexes: [query], blobs: [query, "subject"] }
+												} else {
+														console.log("Unrecognised search filter");
+												}
+										} else {
+												console.log("Unrecognised search request");
+										}
 
-						const headers = new Headers();
-						headers.set("Cache-Control", "max-age=600");
-						headers.append("Content-Type", "application/json");
-						headers.append("Access-Control-Allow-Origin", "*");
-						headers.append("Access-Control-Allow-Methods", "GET,OPTIONS");
+										let response = await fetch(url, {
+												cf: {
+														cacheEverything: true,
+														cacheTtl: 600
+												},
+												headers: {
+														"api-key": "TBapMt2RTuulXdyMMICzPK5Jk2HyHNUXKhWX9Sex9IAzSeBS5J1Z",
+														"content-type": "application/json;charset=UTF-8",
+												},
+												body: requestBody,
+												method: "POST"
+										});
 
-						return new Response(JSON.stringify(body), {headers})
+										const headers = new Headers();
+										headers.set("Cache-Control", "max-age=600");
+										headers.append("Content-Type", "application/json");
+										headers.append("Access-Control-Allow-Origin", "*");
+										headers.append("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+										headers.append("x-flow", "2");
+
+										if (response.status != 200) {
+												return new Response(response.body, { headers, status: response.status })
+										}
+
+										let body: any = await response.json();
+										body["@odata.context"] = null;
+										var bodyJson = JSON.stringify(body);
+
+										if (dataPoint) {
+												if (request.cf) {
+														if (request.cf.city) {
+																var city: string = request.cf.city;
+																dataPoint.blobs?.push(city);
+														}
+														if (request.cf.country) {
+																var country: string = request.cf.country;
+																dataPoint.blobs?.push(country);
+														}
+												}
+												//console.log(JSON.stringify(dataPoint));
+										}
+
+										env.Analytics.writeDataPoint(dataPoint);
+
+										return new Response(bodyJson, { headers })
+								});
 				}
 
 				return new Response(
-						`Call ${searchRoute} to search episode descriptions
-Call ${homeRoute} to get the last 7-days of new releases
+						`Call ${homeRoute} to get the last 7-days of new releases
 ${pathname}`,
 						{
 								headers: {
