fbb792b9b996d7edd2f6418efb415bd18ae85889
Jon Breen <jonbreen@outlook.com>
2024-04-30 18:16:44 +0100

    Add authorization header to cors allow headers

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ export default {
 						"Access-Control-Allow-Origin": "*", //"https://cultpodcasts.com",
 						"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
 						"Access-Control-Max-Age": "86400",
-						"Access-Control-Allow-Headers": "content-type"
+						"Access-Control-Allow-Headers": "content-type,authorization"
 				};
 
 				if (request.method === "OPTIONS") {
