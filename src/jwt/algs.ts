962e10ec63e6e303f5e907af3b9c924a1c30f42c
Jon Breen <jonbreen@outlook.com>
2024-05-01 15:07:48 +0100

    Add package

--- src/jwt/algs.ts
+++
@@ -1,7 +0,0 @@
-export const algToHash: Record<string, string> = {
-  RS256: 'SHA-256',
-  RS384: 'SHA-384',
-  RS512: 'SHA-512'
-};
-
-export const algs = Object.keys(algToHash);
--- src/jwt/index.ts
+++
@@ -1,5 +0,0 @@
-export { decodeJwt } from './decode.js';
-export { getJwks, getKey, importKey } from './jwks.js';
-export { parseJwt } from './parse.js';
-export * from './types.js';
-export { verifyJwtSignature } from './verify.js';
