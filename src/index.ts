3e10c8b3c238486e177e92443346877769e10971
CultPodcastsBot <142722442+cultpodcasts@users.noreply.github.com>
2024-05-03 16:10:22 +0100

    Feature/new router (#20)
    * Add authorization header to cors allow headers
    
    * Set up origin list and added allow credentials
    
    * Set origin throughout
    
    * Add local fqd
    
    * Add JWT handling
    
    * Add package
    
    * Fix reference to lib
    
    * removed Bearer prefix
    
    * local dev environment
    
    * Port over to Hono
    
    * FIx header (likely to remove)
    
    * Clean up
    
    * Deploy from branch
    
    * Add staging branch
    
    ---------
    
    Co-authored-by: Jon Breen <jonbreen@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -1,277 +1,297 @@
-export interface Env {
-		Content: R2Bucket;
-		Data: R2Bucket;
-		Analytics: AnalyticsEngineDataset;
-		DB: D1Database;
-		apikey: string;
-		apihost: string;
-		gatewayKey: string;
+import { parseJwt } from '@cfworker/jwt';
+import { Hono } from 'hono';
+import { cors } from 'hono/cors'
+import { stream } from 'hono/streaming'
+
+type Env = {
+	Content: R2Bucket;
+	Data: R2Bucket;
+	Analytics: AnalyticsEngineDataset;
+	DB: D1Database;
+	apikey: string;
+	apihost: string;
+	gatewayKey: string;
 }
 
-export default {
-		async fetch(request: Request, env: Env) {
-				const leechHandlingActive = false;
-				const { pathname, searchParams } = new URL(request.url);
-				const homeRoute = "/homepage";
-				const searchRoute = "/api";
-				const submitRoute = "/submit";
-				const corsHeaders = {
-						"Access-Control-Allow-Origin": "*", //"https://cultpodcasts.com",
-						"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-						"Access-Control-Max-Age": "86400",
-						"Access-Control-Allow-Headers": "content-type"
-				};
+const allowedOrigins: Array<string> = [
+	"https://cultpodcasts.com",
+	"http://localhost:4200",
+	"https://local.cultpodcasts.com:4200",
+	"https://localhost:4200",
+	"https://feature-api-authorisation.website-83e.pages.dev"
+];
 
-				if (request.method === "OPTIONS") {
-						return new Response(null, {
-								headers: {
-										...corsHeaders,
-								},
-						});
+function getOrigin(origin: string | null | undefined) {
+	if (origin == null || allowedOrigins.indexOf(origin.toLowerCase()) == -1) {
+		origin = allowedOrigins[0];
+	}
+	return origin;
+}
+
+const app = new Hono<{ Bindings: Env }>();
+
+app.use('/*', cors({
+	origin: (origin, c) => {
+		return getOrigin(origin);
+	},
+	allowHeaders: ['content-type', 'authorization'],
+	allowMethods: ['GET', 'HEAD', 'POST', 'OPTIONS'],
+	maxAge: 86400,
+	credentials: true,
+}))
+
+app.get('/homepage', async (c) => {
+	let object: R2ObjectBody | null = null;
+	try {
+		object = await c.env.Content.get("homepage");
+	} catch (e) {
+	}
+
+	if (object === null) {
+		return new Response("Object Not Found", { status: 404 });
+	}
+
+	c.header("etag", object.httpEtag);
+	c.header("Cache-Control", "max-age=600");
+	c.header("Access-Control-Allow-Origin", getOrigin(c.req.header("Origin")));
+	c.header("Access-Control-Allow-Methods", "GET,OPTIONS");
+
+	return stream(c, async (stream) => {
+		stream.onAbort(() => {
+		  console.log('Aborted!')
+		})
+		await stream.pipe(object.body)
+	  })
+});
+
+app.post("/search", async (c) => {
+	const leechHandlingActive: boolean = false;
+	const url = `${c.env.apihost}`;
+	let dataPoint: AnalyticsEngineDataPoint = { indexes: [], blobs: [] };
+	let ipAddress: string = "";
+	let asn: string = "";
+	let city: string = "";
+	if (c.req.raw.cf != undefined && c.req.raw.cf) {
+		dataPoint.blobs!.push(c.req.raw.cf.clientTrustScoretr as string);
+		asn = c.req.raw.cf.asn as string;
+		dataPoint.blobs!.push(asn);
+		ipAddress = c.req.header('cf-connecting-ip') as string
+		dataPoint.blobs!.push(ipAddress);
+		dataPoint.blobs!.push(c.req.header('User-Agent') as string);
+		if (c.req.raw.cf.city) {
+			city = c.req.raw.cf.city as string;
+			dataPoint.blobs!.push(city);
+		}
+		if (c.req.raw.cf.country) {
+			dataPoint.blobs!.push(c.req.raw.cf.country as string);
+		}
+	}
+	let isLeech: boolean = false;
+
+	if (leechHandlingActive) {
+		const object = await c.env.Data.get("leeches");
+
+		if (object != null) {
+			var leeches: string[] = await object.json();
+			console.log(`ip-address: ${ipAddress} index: ${leeches.indexOf(ipAddress)} lookup: ${JSON.stringify(leeches)}`);
+			if (leeches.indexOf(ipAddress) >= 0) {
+				isLeech = true;
+			}
+		}
+	}
+
+	if (!isLeech) {
+		return c.req
+			.json()
+			.then(async (data: any) => {
+				let requestBody = JSON.stringify(data);
+				let index: string = "";
+				if (data.search) {
+					index = data.search;
+					dataPoint.blobs!.push(data.search);
+					dataPoint.blobs!.push("search");
 				}
-
-				if (pathname.startsWith(homeRoute) && request.method === "GET") {
-						const object = await env.Content.get("homepage");
-
-						if (object === null) {
-								return new Response("Object Not Found", { status: 404 });
-						}
-
-						const headers = new Headers();
-						object.writeHttpMetadata(headers);
-						headers.set("etag", object.httpEtag);
-						headers.set("Cache-Control", "max-age=600");
-						headers.append("Access-Control-Allow-Origin", "*");
-						headers.append("Access-Control-Allow-Methods", "GET,OPTIONS");
-
-						return new Response(object.body, { headers });
-				}
-
-				if (pathname.startsWith(searchRoute) && request.method === "POST") {
-						const url = `${env.apihost}`;
-						let dataPoint: AnalyticsEngineDataPoint = { indexes: [], blobs: [] };
-						let ipAddress: string = "";
-						let asn: string = "";
-						let city: string = "";
-						if (request.cf) {
-								dataPoint.blobs!.push(request.cf.clientTrustScoretr as string);
-								asn = request.cf.asn as string;
-								dataPoint.blobs!.push(asn);
-								ipAddress = request.headers.get('cf-connecting-ip') as string
-								dataPoint.blobs!.push(ipAddress);
-								dataPoint.blobs!.push(request.headers.get('User-Agent') as string);
-								if (request.cf.city) {
-										city = request.cf.city as string;
-										dataPoint.blobs!.push(city);
-								}
-								if (request.cf.country) {
-										dataPoint.blobs!.push(request.cf.country as string);
-								}
-						}
-						let isLeech: boolean = false;
-
-						if (leechHandlingActive) {
-								const object = await env.Data.get("leeches");
-
-								if (object != null) {
-										var leeches: string[] = await object.json();
-										console.log(`ip-address: ${ipAddress} index: ${leeches.indexOf(ipAddress)} lookup: ${JSON.stringify(leeches)}`);
-										if (leeches.indexOf(ipAddress) >= 0) {
-												isLeech = true;
-										}
-								}
-						}
-
-						if (!isLeech) {
-								return request
-										.json()
-										.then(async (data: any) => {
-												let requestBody = JSON.stringify(data);
-												let index: string = "";
-												if (data.search) {
-														index = data.search;
-														dataPoint.blobs!.push(data.search);
-														dataPoint.blobs!.push("search");
-												}
-												if (data.filter) {
-														let filter: string = data.filter;
-														if (filter.indexOf("(podcastName eq '") == 0) {
-																let query = filter.slice(17, -2);
-																if (index) {
-																		index += " podcast=" + query;
-																} else {
-																		index = "podcast=" + query;
-																}
-																dataPoint.blobs!.push(query);
-																dataPoint.blobs!.push("podcast");
-														} else if (filter.indexOf("subjects/any(s: s eq '") == 0) {
-																let query = filter.slice(22, - 2);
-																if (index) {
-																		index += " subject=" + query;
-																} else {
-																		index = "subject=" + query;
-																}
-																dataPoint.blobs!.push(query);
-																dataPoint.blobs!.push("subject");
-														} else {
-																console.log("Unrecognised search filter");
-														}
-												}
-												dataPoint.indexes!.push(index);
-
-												if (!data.search && !data.filter) {
-														console.log("Unrecognised search request");
-												}
-												if (dataPoint) {
-														dataPoint.blobs?.push(data.skip);
-														dataPoint.blobs?.push(data.orderby);
-												}
-
-
-												let response = await fetch(url, {
-														cf: {
-																cacheEverything: true,
-																cacheTtl: 600
-														},
-														headers: {
-																"api-key": env.apikey,
-																"content-type": "application/json;charset=UTF-8",
-														},
-														body: requestBody,
-														method: "POST"
-												});
-												if (dataPoint) {
-														dataPoint.blobs?.push(response.status.toString());
-														env.Analytics.writeDataPoint(dataPoint);
-												}
-
-												const headers = new Headers();
-												headers.set("Cache-Control", "max-age=600");
-												headers.append("Content-Type", "application/json");
-												headers.append("Access-Control-Allow-Origin", "*");
-												headers.append("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-
-												if (response.status != 200) {
-														return new Response(response.body, { headers, status: response.status })
-												}
-
-												let body: any = await response.json();
-												body["@odata.context"] = null;
-												return new Response(JSON.stringify(body), { headers })
-										});
+				if (data.filter) {
+					let filter: string = data.filter;
+					if (filter.indexOf("(podcastName eq '") == 0) {
+						let query = filter.slice(17, -2);
+						if (index) {
+							index += " podcast=" + query;
 						} else {
-								const leechResponse = {
-										"@odata.context": null,
-										"@odata.count": 1,
-										"@search.facets": {
-												"subjects": [],
-												"podcastName": []
-										},
-										"value": [{
-												"@search.score": 1.0,
-												"id": "00000000-0000-0000-0000-000000000000",
-												"episodeTitle": "Leech Detected",
-												"podcastName": "Leech Detected",
-												"episodeDescription": "Contact leeching@cultpodcasts.com.",
-												"release": "1970-01-01T00:00:00Z",
-												"duration": "01:00:00.0000000",
-												"explicit": false,
-												"spotify": null,
-												"apple": null,
-												"youtube": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-												"subjects": []
-										}]
-								};
-								const headers = new Headers();
-								headers.set("Cache-Control", "max-age=600");
-								headers.append("Content-Type", "application/json");
-								headers.append("Access-Control-Allow-Origin", "*");
-								headers.append("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-
-								dataPoint.blobs!.push("Leech");
-								env.Analytics.writeDataPoint(dataPoint);
-								var response = new Response(JSON.stringify(leechResponse), { headers });
-								return response;
+							index = "podcast=" + query;
 						}
+						dataPoint.blobs!.push(query);
+						dataPoint.blobs!.push("podcast");
+					} else if (filter.indexOf("subjects/any(s: s eq '") == 0) {
+						let query = filter.slice(22, - 2);
+						if (index) {
+							index += " subject=" + query;
+						} else {
+							index = "subject=" + query;
+						}
+						dataPoint.blobs!.push(query);
+						dataPoint.blobs!.push("subject");
+					} else {
+						console.log("Unrecognised search filter");
+					}
+				}
+				dataPoint.indexes!.push(index);
+
+				if (!data.search && !data.filter) {
+					console.log("Unrecognised search request");
+				}
+				if (dataPoint) {
+					dataPoint.blobs?.push(data.skip);
+					dataPoint.blobs?.push(data.orderby);
 				}
 
-				if (pathname.startsWith(submitRoute)) {
-						if (request.method === "POST") {
-
-								const headers = new Headers();
-								headers.set("Cache-Control", "max-age=600");
-								headers.append("Content-Type", "application/json");
-								headers.append("Access-Control-Allow-Origin", "*");
-								headers.append("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-
-								return request
-										.json()
-										.then(async (data: any) => {
-												let url: URL | undefined;
-												let urlParam = data.url;
-												if (urlParam == null) {
-														return new Response(JSON.stringify({ error: "Missing url param." }), { headers, status: 400 });
-												}
-												try {
-														url = new URL(urlParam);
-												} catch {
-														return new Response(JSON.stringify({ error: `Invalid url '${data.url}'.` }), { headers, status: 400 });
-												}
-												let insert = env.DB
-														.prepare("INSERT INTO urls (url, timestamp, timestamp_date, ip_address, country, user_agent) VALUES (?, ?, ?, ?, ?, ?)")
-														.bind(url.toString(), Date.now(), new Date().toLocaleString(), request.headers.get("CF-Connecting-IP"), request.headers.get("CF-IPCountry"), request.headers.get("User-Agent"));
-												let result = await insert.run();
-
-												if (result.success) {
-														return new Response(JSON.stringify({ success: "Submitted" }), { headers });
-												} else {
-														return new Response(JSON.stringify({ error: "Unable to accept" }), { headers, status: 400 });
-												}
-										});
-						} else if (request.method === "GET" && request.headers.get("key") === env.gatewayKey) {
-								let submissionIds = env.DB
-										.prepare("SELECT id FROM urls WHERE state=0");
-								let result = await submissionIds.all();
-								if (result.success) {
-										const inClause = result.results
-												.map((urlId) => {
-														if (!Number.isInteger(urlId.id)) { throw Error("invalid id, expected an integer"); }
-														return urlId.id;
-												})
-												.join(',');
-										let urls = "SELECT id, url, timestamp_date, ip_address, country, user_agent FROM urls WHERE id IN ($urlIds)";
-										urls = urls.replace('$urlIds', inClause);
-										let urlResults = await env.DB
-												.prepare(urls)
-												.run();
-										if (urlResults.success) {
-												let update = "UPDATE urls SET state=1 WHERE id IN ($urlIds)";
-												update = update.replace('$urlIds', inClause);
-												let raiseState = await env.DB
-														.prepare(update)
-														.run();
-												if (raiseState.success) {
-														return new Response(JSON.stringify(urlResults.results));
-												} else {
-														return new Response("Failure to raise state of new submissons in ids " + result.results.join(", "), { status: 400 })
-												}
-										} else {
-												return new Response("Unable to retrieve new submissions", { status: 500 });
-										}
-								} else {
-										return new Response(result.error, { status: 500 });
-								}
-						}
+				let response = await fetch(url, {
+					cf: {
+						cacheEverything: true,
+						cacheTtl: 600
+					},
+					headers: {
+						"api-key": c.env.apikey,
+						"content-type": "application/json;charset=UTF-8",
+					},
+					body: requestBody,
+					method: "POST"
+				});
+				if (dataPoint) {
+					dataPoint.blobs?.push(response.status.toString());
+					c.env.Analytics.writeDataPoint(dataPoint);
 				}
 
-				return new Response(
-						`Call ${homeRoute} to get the last 7-days of new releases
-${pathname}`,
-						{
-								headers: {
-										...corsHeaders,
-								},
-						}
-				);
-		},
-};
+				c.header("Cache-Control", "max-age=600");
+				c.header("Content-Type", "application/json");
+				c.header("Access-Control-Allow-Origin", getOrigin(c.req.header("Origin")));
+				c.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+
+				if (response.status != 200) {
+					return c.json(response.body, 400)
+				}
+
+				let body: any = await response.json();
+				body["@odata.context"] = null;
+				return c.json(body, 200);
+			});
+	} else {
+		const leechResponse = {
+			"@odata.context": null,
+			"@odata.count": 1,
+			"@search.facets": {
+				"subjects": [],
+				"podcastName": []
+			},
+			"value": [{
+				"@search.score": 1.0,
+				"id": "00000000-0000-0000-0000-000000000000",
+				"episodeTitle": "Leech Detected",
+				"podcastName": "Leech Detected",
+				"episodeDescription": "Contact leeching@cultpodcasts.com.",
+				"release": "1970-01-01T00:00:00Z",
+				"duration": "01:00:00.0000000",
+				"explicit": false,
+				"spotify": null,
+				"apple": null,
+				"youtube": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+				"subjects": []
+			}]
+		};
+		c.header("Cache-Control", "max-age=600");
+		c.header("Content-Type", "application/json");
+		c.header("Access-Control-Allow-Origin", getOrigin(c.req.header("Origin")));
+		c.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+
+		dataPoint.blobs!.push("Leech");
+		c.env.Analytics.writeDataPoint(dataPoint);
+		return c.json(leechResponse, 200);
+	}
+});
+
+app.get("/submit", async (c) => {
+	if (c.req.header("key") != c.env.gatewayKey) {
+		return c.json({ message: "Unauthorised" }, 401);
+	}
+	let submissionIds = c.env.DB
+		.prepare("SELECT id FROM urls WHERE state=0");
+	let result = await submissionIds.all();
+	if (result.success) {
+		const inClause = result.results
+			.map((urlId) => {
+				if (!Number.isInteger(urlId.id)) { throw Error("invalid id, expected an integer"); }
+				return urlId.id;
+			})
+			.join(',');
+		let urls = "SELECT id, url, timestamp_date, ip_address, country, user_agent FROM urls WHERE id IN ($urlIds)";
+		urls = urls.replace('$urlIds', inClause);
+		let urlResults = await c.env.DB
+			.prepare(urls)
+			.run();
+		if (urlResults.success) {
+			let update = "UPDATE urls SET state=1 WHERE id IN ($urlIds)";
+			update = update.replace('$urlIds', inClause);
+			let raiseState = await c.env.DB
+				.prepare(update)
+				.all();
+			if (raiseState.success) {
+				return c.json(urlResults);
+			} else {
+				return c.text("Failure to raise state of new submissons in ids " + result.results.join(", "), 400);
+			}
+		} else {
+			return c.text("Unable to retrieve new submissions", 500);
+		}
+	} else {
+		return c.text(result.error!, 500);
+	}
+});
+
+app.post("/submit", async (c) => {
+	c.header("Cache-Control", "max-age=600");
+	c.header("Content-Type", "application/json");
+	c.header("Access-Control-Allow-Origin", getOrigin(c.req.header("Origin")));
+	c.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+
+	return c.req
+		.json()
+		.then(async (data: any) => {
+			let url: URL | undefined;
+			let urlParam = data.url;
+			if (urlParam == null) {
+				return c.json({ error: "Missing url param." }, 400);
+			}
+			try {
+				url = new URL(urlParam);
+			} catch {
+				return c.json({ error: `Invalid url '${data.url}'.` }, 400);
+			}
+			let insert = c.env.DB
+				.prepare("INSERT INTO urls (url, timestamp, timestamp_date, ip_address, country, user_agent) VALUES (?, ?, ?, ?, ?, ?)")
+				.bind(url.toString(), Date.now(), new Date().toLocaleString(), c.req.header("CF-Connecting-IP"), c.req.header("CF-IPCountry"), c.req.header("User-Agent"));
+			let result = await insert.run();
+
+			if (result.success) {
+				return c.json({ success: "Submitted" });
+			} else {
+				return c.json({ error: "Unable to accept" }, 400);
+			}
+		});
+});
+
+export default app;
+
+async function auth(request: Request) {
+	const jwt = request.headers.get('Authorization');
+	if (jwt) {
+		const issuer = 'https://dev-q3x2z6aofdzbjkkf.us.auth0.com/';
+		const audience = 'https://api.cultpodcasts.com/';
+
+		const result = await parseJwt(jwt.slice(7), issuer, audience);
+		if (!result.valid) {
+			console.log(result.reason);
+		} else {
+			console.log(result.payload); // { iss, sub, aud, iat, exp, ...claims }
+		}
+	}
+}
