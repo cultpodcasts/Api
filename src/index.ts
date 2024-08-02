e28c2856fd2db80148c5130945977c221e72222b
Jon Breen <jonbreen@outlook.com>
2024-08-02 17:13:01 +0100

    Added dialog close

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ type Env = {
 	auth0Issuer: string;
 	auth0Audience: string;
 	secureSubmitEndpoint: URL;
-	securePodcastsEndpoint: URL;
+	secureEpisodeEndpoint: URL;
 	secureDiscoveryCurationEndpoint: URL;
 }
 
@@ -313,7 +313,7 @@ app.post("/submit", auth0Middleware, async (c) => {
 				'Authorization': authorisation,
 				"Content-type": "application/json",
 				"Cache-Control": "no-cache",
-				"User-Agent": "cultvault-podcasts-api",
+				"User-Agent": "cult-podcasts-api",
 				"Host": new URL(c.env.secureSubmitEndpoint).host
 			},
 			body: JSON.stringify(data),
@@ -363,6 +363,42 @@ app.post("/submit", auth0Middleware, async (c) => {
 	return c.json({ success: "Submitted" });
 });
 
+app.get("/episode/:id", auth0Middleware, async (c) => {
+	const auth0Payload: Auth0JwtPayload = c.var.auth0('payload');
+	const id = c.req.param('id')
+	c.header("Cache-Control", "max-age=600");
+	c.header("Content-Type", "application/json");
+	c.header("Access-Control-Allow-Origin", getOrigin(c.req.header("Origin")));
+	c.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+
+	if (auth0Payload?.permissions && auth0Payload.permissions.includes('curate')) {
+		const authorisation: string = c.req.header("Authorization")!;
+		console.log(`Using auth header '${authorisation.slice(0, 20)}..'`);
+		const url= `${c.env.secureEpisodeEndpoint}/${id}`;
+		console.log(url);
+		const resp = await fetch(url, {
+			headers: {
+				'Accept': "*/*",
+				'Authorization': authorisation,
+				"Content-type": "application/json",
+				"Cache-Control": "no-cache",
+				"User-Agent": "cult-podcasts-api",
+				"Host": new URL(c.env.secureEpisodeEndpoint).host
+			},
+			method: "GET"
+		});
+		if (resp.status == 200) {
+			console.log(`Successfully used secure-episode-endpoint.`);
+
+			return new Response(resp.body);
+		} else {
+			console.log(`Failed to use secure-podcasts-endpoint. Response code: '${resp.status}'.`);
+		}
+
+	}
+	return c.json({ error: "Unauthorised" }, 403);
+});
+
 app.get("/discovery-curation", auth0Middleware, async (c) => {
 	const auth0Payload: Auth0JwtPayload = c.var.auth0('payload');
 	c.header("Cache-Control", "max-age=600");
@@ -379,7 +415,7 @@ app.get("/discovery-curation", auth0Middleware, async (c) => {
 				'Authorization': authorisation,
 				"Content-type": "application/json",
 				"Cache-Control": "no-cache",
-				"User-Agent": "cultvault-podcasts-api",
+				"User-Agent": "cult-podcasts-api",
 				"Host": new URL(c.env.secureDiscoveryCurationEndpoint).host
 			},
 			method: "GET"
@@ -417,7 +453,7 @@ app.post("/discovery-curation", auth0Middleware, async (c) => {
 					'Authorization': authorisation,
 					"Content-type": "application/json",
 					"Cache-Control": "no-cache",
-					"User-Agent": "cultvault-podcasts-api",
+					"User-Agent": "cult-podcasts-api",
 					"Host": new URL(c.env.secureDiscoveryCurationEndpoint).host
 				},
 				method: "POST",
