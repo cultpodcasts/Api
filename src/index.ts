0fb9d2e98bbe1924d3e039557f262c9babef180e
Jon Breen <jonbreen@outlook.com>
2024-08-11 18:37:03 +0100

    Add episode post endpoint

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ type Env = {
 	securePodcastEndpoint: URL;
 	secureSubjectEndpoint: URL;
 	secureEpisodesOutgoingEndpoint: URL;
+	secureEpisodePublishEndpoint: URL;
 }
 
 const allowedOrigins: Array<string> = [
@@ -446,6 +447,42 @@ app.post("/episode/:id", auth0Middleware, async (c) => {
 				"Cache-Control": "no-cache",
 				"User-Agent": "cult-podcasts-api",
 				"Host": new URL(c.env.secureEpisodeEndpoint).host
+			},
+			method: "POST",
+			body: body
+		});
+		if (resp.status == 202) {
+			console.log(`Successfully used secure-episode-endpoint.`);
+			return new Response(resp.body);
+		} else {
+			console.log(`Failed to use secure-episode-endpoint. Response code: '${resp.status}'.`);
+			return c.json({ error: "Error" }, 500);
+		}
+	}
+	return c.json({ error: "Unauthorised" }, 403);
+});
+
+app.post("/episode/publish/:id", auth0Middleware, async (c) => {
+	const auth0Payload: Auth0JwtPayload = c.var.auth0('payload');
+	const id = c.req.param('id')
+	c.header("Cache-Control", "max-age=600");
+	c.header("Content-Type", "application/json");
+	c.header("Access-Control-Allow-Origin", getOrigin(c.req.header("Origin")));
+	c.header("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+
+	if (auth0Payload?.permissions && auth0Payload.permissions.includes('curate')) {
+		const authorisation: string = c.req.header("Authorization")!;
+		const url = `${c.env.secureEpisodePublishEndpoint}/${id}`;
+		const data: any = await c.req.json();
+		const body: string = JSON.stringify(data);
+		const resp = await fetch(url, {
+			headers: {
+				'Accept': "*/*",
+				'Authorization': authorisation,
+				"Content-type": "application/json",
+				"Cache-Control": "no-cache",
+				"User-Agent": "cult-podcasts-api",
+				"Host": new URL(c.env.secureEpisodePublishEndpoint).host
 			},
 			method: "POST",
 			body: body
