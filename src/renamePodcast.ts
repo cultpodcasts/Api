d92476d810ac5197a863c12ab675a3004948407e
Jon Breen <jonbreen@outlook.com>
2024-10-09 15:28:59 +0100

    Added handling for response

--- src/renamePodcast.ts
+++ src/renamePodcast.ts
@@ -5,23 +5,29 @@ import { buildFetchHeaders } from "./buildFetchHeaders";
 
 export async function renamePodcast(c: Auth0ActionContext): Promise<Response> {
     const auth0Payload: Auth0JwtPayload = c.var.auth0('payload');
-    const podcastName = c.req.param('podcastName');
+    const podcastName = c.req.param('name');
     const newName = c.req.param('newName');
-    AddResponseHeaders(c, { methods: ["PATCH", "GET", "OPTIONS"] });
-    if (auth0Payload?.permissions && auth0Payload.permissions.includes('curate')) {
-        const url = `${c.env.securePodcastEndpoint}/${podcastName}/name/${newName}`;
+    AddResponseHeaders(c, { methods: ["POST", "GET", "OPTIONS"] });
+    if (auth0Payload?.permissions && auth0Payload.permissions.includes('admin')) {
+        const url = `${c.env.securePodcastEndpoint}/name/${podcastName}`;
         const data: any = await c.req.json();
         const body: string = JSON.stringify(data);
         const resp = await fetch(url, {
             headers: buildFetchHeaders(c.req, c.env.securePodcastEndpoint),
-            method: "PATCH",
+            method: "POST",
             body: body
         });
         if (resp.status == 200) {
             console.log(`Successfully used secure-podcast-endpoint to rename podcast.`);
             return new Response(resp.body);
-        } else if (resp.status == 404) {
+        } else if (resp.status == 400) {
             console.log(`Unable to find podcast to rename podcast.`);
+            return new Response(resp.body, { status: resp.status });
+        } else if (resp.status == 404) {
+            console.log(`Unable to find podcast to rename podcast. Podccast not found.`);
+            return new Response(resp.body, { status: resp.status });
+        } else if (resp.status == 409) {
+            console.log(`Unable to find podcast to rename podcast. Podcast exists with new-name.`);
             return new Response(resp.body, { status: resp.status });
         } else {
             console.log(`Failed to use secure-podcast-endpoint to rename podcast. Response code: '${resp.status}'.`);
