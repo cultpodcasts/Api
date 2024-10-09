97332f4448f9eb3d9485f4107a97155d24d4e621
Jon Breen <jonbreen@outlook.com>
2024-10-09 15:29:30 +0100

    Updated route

--- src/index.ts
+++ src/index.ts
@@ -48,6 +48,6 @@ app.post("/discovery-curation", Auth0Middleware, submitDiscovery);
 app.post("/searchindex/run", Auth0Middleware, runSearchIndexer);
 app.post("/publish/homepage", Auth0Middleware, publishHomepage);
 app.post("/terms", Auth0Middleware, publishTerm);
-app.patch("/podcast/:podcastName/name/:newName", Auth0Middleware, renamePodcast)
+app.post("/podcast/name/:name", Auth0Middleware, renamePodcast)
 
 export default app;
