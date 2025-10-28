c4cf8cb71d7078bde0c60d05c2fa81b849789cc8
MSI\jonbr <cultpodcasts@educocult.com>
2025-10-28 17:11:45 +0000

    Bump packages

--- src/Auth0Middleware.ts
+++ src/Auth0Middleware.ts
@@ -7,17 +7,24 @@ import { Context, Next } from "hono";
 export const Auth0Middleware = createMiddleware<AppContext>(async (c: Context<AppContext, any, {}>, next: Next) => {
 	const authorization = c.req.header('Authorization');
 	const bearer = "Bearer ";
-	c.set('auth0', (payload) => { });
-	if (authorization && authorization.startsWith(bearer)) {
-		const token = authorization.slice(bearer.length);
-		const result = await parseJwt(token, c.env.auth0Issuer, c.env.auth0Audience);
-		if (result.valid) {
-			c.set('auth0', (payload) => result.payload as Auth0JwtPayload);
-		} else {
-			console.error({ message: "JWT invalid", reason: result.reason });
-		}
+	if (!c.env.auth0Issuer || !c.env.auth0Audience) {
+		console.error({ message: "Auth0 not configured" });
 	} else {
-		console.error({ message: "no bearer" });
-	}
+		c.set('auth0', (payload) => { });
+		if (authorization && authorization.startsWith(bearer)) {
+			const token = authorization.slice(bearer.length);
+			const result = await parseJwt(token, c.env.auth0Issuer, c.env.auth0Audience);
+			if (result.valid) {
+				c.set('auth0', (payload) => result.payload as Auth0JwtPayload);
+			} else {
+				console.error({ message: "JWT invalid", result: result });
+			}
+		} else {
+			console.error({ message: "no bearer" });
+		}
 	await next();
+	return;
+	}
+	return new Response('Configure error (1)', { status: 500 });
+
 });
